22b2b8291d0d800794e80b65e30b7b20929e1ea0
jankomp <jankompatscher@gmail.com>
2025-04-07 18:14:19 +0300

    create logs summary scalars

--- create_logs_summary_scalars.py
+++ create_logs_summary_scalars.py
@@ -5,14 +5,16 @@ from tensorboard.backend.event_processing.event_accumulator import EventAccumula
 import matplotlib.pyplot as plt
 
 # The parent directory where your log files are stored
-log_dir = 'compare_feedback_types/tb_logs'
-environment_short_name = 'Walker2d'
+log_dir = 'tb_logs'
+environment_short_name = 'Hopper'
 groupwise_number_of_runs = 5
 pairwise_number_of_runs = 5
+visualization_number_of_runs = 5
 
 # The names of the runs you want to process
 groupwise_runs = [f'groupwise_{i}_{environment_short_name}_0' for i in range(groupwise_number_of_runs)]
 pairwise_runs = [f'pairwise_{i}_{environment_short_name}_0' for i in range(pairwise_number_of_runs)]
+visualization_runs = [f'visualization_{i}_{environment_short_name}_0' for i in range(visualization_number_of_runs)]
 
 # The name of the scalar you want to process
 scalar_name = 'rollout/ep_rew_mean'
@@ -49,14 +51,16 @@ def process_runs(runs, scalar_name, window_size=1):
 # Process the runs
 groupwise_steps, groupwise_mean, groupwise_std = process_runs(groupwise_runs, scalar_name)
 pairwise_steps, pairwise_mean, pairwise_std = process_runs(pairwise_runs, scalar_name)
+visualization_steps, visualization_mean, visualization_std = process_runs(visualization_runs, scalar_name)
 
 print(f'Final true reward for groupwise: {groupwise_mean[-1]}')
 print(f'Final true reward for pairwise: {pairwise_mean[-1]}')
+print(f'Final true reward for visualization: {visualization_mean[-1]}')
 
 # Create a new figure
 plt.figure()
 
-plt.title(f'{environment_short_name} - Groupwise vs Pairwise Feedback')
+plt.title(f'{environment_short_name} - Visualization vs Groupwise vs Pairwise Feedback')
 
 #label the axes
 plt.xlabel('Steps')
@@ -69,6 +73,10 @@ plt.fill_between(groupwise_steps, groupwise_mean - groupwise_std, groupwise_mean
 # Plot the mean and standard deviation for pairwise
 plt.plot(pairwise_steps, pairwise_mean, label=f'Pairwise Mean accross {pairwise_number_of_runs} runs')
 plt.fill_between(pairwise_steps, pairwise_mean - pairwise_std, pairwise_mean + pairwise_std, alpha=0.1)
+
+# Plot the mean and standard deviation for visualization
+plt.plot(visualization_steps, visualization_mean, label=f'Visualization Mean across {visualization_number_of_runs} runs')
+plt.fill_between(visualization_steps, visualization_mean - visualization_std, visualization_mean + visualization_std, alpha=0.1)
 
 # Add a legend
 plt.legend(loc='lower right')
